157a33089c19717f32f6925d45382ab2d5ac429e
Property404 <daganboy@gmail.com>
2018-01-20 01:21:08 -0500

    Location nonsense works

--- app/src/main/java/com/example/tareg/seedeway/MainActivity.java
+++ app/src/main/java/com/example/tareg/seedeway/MainActivity.java
@@ -1,70 +1,104 @@
 package com.example.tareg.seedeway;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity{
     private TextView positionView;
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set up layout resources
         positionView = findViewById(R.id.position);
+
+        // Request all permissions needed
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
+
+        // Try to get orientation sensors
         try {
             mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
             mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
-        }catch(Exception e){
+        } catch (Exception e) {
             Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
         }
 
-        requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0,
-                locationListener);
-    }
+        // Location nonsense
+        locationManager = (LocationManager)
+                getSystemService(this.LOCATION_SERVICE);
+        locationListener = new MyLocationListener();
 
-    @Override
-    public void onLocationChanged(Location location) {
-        try {
-            String msg = location.getLatitude()
-                    + "/" + location.getLongitude();
 
-            positionView.setText(msg);
-
-        }catch(Exception e){
-            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        // More location nonsense
+        if ( PermissionChecker.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED ) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+            Toast.makeText(this, "granted", Toast.LENGTH_LONG).show();
+        }else {
+            Toast.makeText(this, "fuckadoodledo", Toast.LENGTH_LONG).show();
         }
     }
+    private class MyLocationListener implements LocationListener {
 
-    @Override
-    public void onProviderDisabled(String provider) {
+        @Override
+        public void onLocationChanged(Location location) {
+            try {
+                String msg = location.getLatitude()
+                        + "/" + location.getLongitude();
 
-        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-        startActivity(intent);
-        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
-                Toast.LENGTH_SHORT).show();
-    }
+                positionView.setText(msg);
+                Toast.makeText(getBaseContext(), "!", Toast.LENGTH_LONG).show();
+            }catch(Exception e){
+                Toast.makeText(getBaseContext(), e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
 
-    @Override
-    public void onProviderEnabled(String provider) {
+        @Override
+        public void onProviderDisabled(String provider) {
 
-        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
-                Toast.LENGTH_SHORT).show();
-    }
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(intent);
+            Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                    Toast.LENGTH_SHORT).show();
+        }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
+        @Override
+        public void onProviderEnabled(String provider) {
 
+            Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            Toast.makeText(getBaseContext(), "j" , Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
+
