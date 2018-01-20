27463773d3ec6fb105724f864b599159d1649479
Property404 <daganboy@gmail.com>
2018-01-19 23:28:35 -0500

    Add position indicators

--- app/src/main/java/com/example/tareg/seedeway/MainActivity.java
+++ app/src/main/java/com/example/tareg/seedeway/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.tareg.seedeway;
 
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    TextView positionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        positionView = findViewById(R.id.position);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        String msg = location.getLatitude()
+                + "/" + location.getLongitude();
+
+        positionView.setText(msg);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+
     }
 }
